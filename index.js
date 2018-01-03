40457661166dc7c637d74ea7bf1485a94e8e8cfd
Fellipe Caetano <fellipe.caetano4@gmail.com>
2018-01-03 10:27:53 -0200

    React to exceptions

--- index.js
+++ index.js
@@ -15,28 +15,35 @@ program
   .action(async options => {
     const config = createConfiguration(options.config);
     const browser = await puppeteer.launch(config.browser);
-    const page = await browser.newPage();
 
-    await page.goto(config.page.url, {
-      waitUntil: 'domcontentloaded'
-    });
+    try {
+      const page = await browser.newPage();
 
-    // Authenticate the device
-    await page.evaluate(config => {
-      localStorage.pontoweb_identity = config.device.identity;
-      localStorage.company_name = config.device.companyName;
-    }, config);
+      await page.goto(config.page.url, {
+        waitUntil: 'domcontentloaded'
+      });
 
-    await page.click(selectors.account);
-    await page.keyboard.type(config.credentials.account);
+      // Authenticate the device
+      await page.evaluate(config => {
+        localStorage.pontoweb_identity = config.device.identity;
+        localStorage.company_name = config.device.companyName;
+      }, config);
 
-    await page.click(selectors.password);
-    await page.keyboard.type(config.credentials.password);
+      await page.click(selectors.account);
+      await page.keyboard.type(config.credentials.account);
 
-    await page.click(selectors.button);
-    await page.waitFor(selectors.successMessage);
+      await page.click(selectors.password);
+      await page.keyboard.type(config.credentials.password);
 
-    await browser.close();
+      // Apparently the first click only focuses the button,
+      // and two clicks are needed to submit the form
+      await page.click(selectors.button);
+      await page.click(selectors.button);
+
+      await page.waitFor(selectors.successMessage);
+    } finally {
+      await browser.close();
+    }
   });
 
 program.parse(process.argv);
